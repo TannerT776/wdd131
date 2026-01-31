a3c6f98c433c2c03ac996fe8c0e61df60ca5c5ef
TannerT776 <ttinsley7762001@gmail.com>
2026-01-30 19:44:05 -0700

    Add responsive menu toggle functionality

---
+++ ponder/responsive-menu.js
@@ -0,0 +1,7 @@
+const menuButton = document.querySelector('#menu-button');
+const nav = document.querySelector('nav');
+
+menuButton.addEventListener('click', () => {
+  nav.classList.toggle('hide');
+  menuButton.classList.toggle('change');
+});
